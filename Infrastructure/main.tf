af88ed0d9fc2944c58bb174513264c2dcc48e4dd
RUTH-HELENE <ruthhelene2@gmail.com>
2026-03-16 07:16:12 -0400

    10- Déploiement d’un container Docker à partir de votre pipeline dans MS Azure

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -62,3 +62,30 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
+
+
+
+# 10- Déploiement d’un container Docker à partir de votre pipeline dans MS Azure.
+resource "azurerm_container_group" "aci" {
+  name                = "aci-docker-Ruth"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-poly-Ruth-Helene-Moulen" # Changez ceci pour l'unicité
+  os_type             = "Linux"
+ 
+  container {
+    name   = "hello-world"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+ 
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
+ 
+
